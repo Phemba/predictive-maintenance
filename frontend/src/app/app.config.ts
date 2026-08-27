b44c4cec8790c72b672a20e467a54ecc32220174
Phemba <215414736@tut4life.ac.za>
2026-08-27 18:30:03 +0200

    Fix ngSubmit binding, add form styling

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 
 
 import { routes } from './app.routes';
+// Step4: Import the HttpClient module to enable HTTP requests
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
--- frontend/src/app/predict-form/predict-form.ts
+++ frontend/src/app/predict-form/predict-form.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule } from '@angular/common'; 
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
+// Standalone component for the predictive maintenance form
 @Component({
   imports: [CommonModule, FormsModule],
   selector: 'app-predict-form',
@@ -12,7 +13,7 @@ import { HttpClient } from '@angular/common/http';
   
 })
 export class PredictForm {
-  //Matching backend fields
+  // Step1: properties matching the model backend fields
   type: number = 0;
   airTemperature: number = 298;
   processTemperature: number = 308;
@@ -20,22 +21,27 @@ export class PredictForm {
   torque: number = 40;
   toolWear: number = 0;
 
+  // Step 2: Property to hold the prediction once we get one
   riskResult: number | null = null;
 
-  //FastAPI request from frontend to backend
+  //Dependency injection of HttpClient to make HTTP requests to the backend.
   constructor (private http: HttpClient) {}
 
+  //Step5: Method to handle form submission and send data to the backend
+  //Use snake_case for the payload keys to match the backend model
   onSubmit() {
     const payload = {
       type: this.type,
-      airTemperature: this.airTemperature,
-      processTemperature: this.processTemperature,
-      rotationalSpeed: this.rotationalSpeed,
+      air_temperature: this.airTemperature,
+      process_temperature: this.processTemperature,
+      rotational_speed: this.rotationalSpeed,
       torque: this.torque,
-      toolWear: this.toolWear
+      tool_wear: this.toolWear
     };
-    // Send the payload to the FastAPI backend
-    // use .subscribe to handle the response
+    // .subscribe({next, error}) HTTP calls take time to complete,
+    // use .subscribe to tell Angular "Run this code once the response comes back"
+    // .next handles the successful response, .error handles any errors that occur during the request.
+
     this.http.post<{ risk: number }>('http://localhost:8000/predict', payload)
     .subscribe({
        next: (response) => {
